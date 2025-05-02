408e56002a84b152d79619399760254fcb8414c1
Ruilopes31 <108439832+Ruilopes31@users.noreply.github.com>
2025-05-02 19:50:59 +0000

    salvar

--- vale.py
+++ vale.py
@@ -6,3 +6,10 @@ desconto = salario_bruto * 0.06
 
 
 print(f"O valor do vale-transporte a ser descontado é: R$ {valor_vale_transporte:.2f}")
+
+
+valor_total = valor_refeicao * 0.20
+print(f"O valor total do vale refeição é: R${valor_total:.2f}")
+
+def calcular_refeicao
+valor_total = valor_refeicao * 0.20
