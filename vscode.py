ad64e9d141a03321fc7e06f710c373a664f1715a
Ruilopes31 <108439832+Ruilopes31@users.noreply.github.com>
2025-05-20 18:24:40 +0000

    salvar

--- vscode.py
+++ vscode.py
@@ -22,4 +22,24 @@ def cadastrar_fun(lista_funcionario):
     
     lista_funcionario.append(funcionario)
     print()
-    
+
+
+
+
+
+def atualizar(lista):
+    if verificar_lista_vazia(lista):
+        return
+   
+    nome_atualizar = input("Digite o nome do funcionário que deseja atualizar: ")
+    encontrado = False
+   
+    for funcionario in lista:
+        if funcionario.nome == nome_atualizar:
+            print("= Digite os dados do funcionário = ")
+            funcionario.nome=input("Nome: "),
+            funcionario.cpf=input("CPF: "),
+            funcionario.cargo=input("Cargo: "),
+            funcionario.salario=input("Salario: ")
+            encontrado = True
+            break
